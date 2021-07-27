97263924e55cf622077d983724eb031419527ee3
Kyle Tu <kyle.tu@sjsu.edu>
2021-07-27 11:04:19 -0700

    new url in file

--- client/src/reducers/constants.js
+++ client/src/reducers/constants.js
@@ -1,7 +1,8 @@
 // Use this string for API
 
-export const API_URL =
+export const API_URL = 
   "https://ht56vci4v9.execute-api.us-west-1.amazonaws.com/dev/api/v2/";
+  // "https://c1zwsl05s5.execute-api.us-west-1.amazonaws.com/dev/api/v2"
 // export const API_URL = "http://localhost:2000/api/v2/";
 export const BING_LOCATION_API_URL =
   "https://dev.virtualearth.net/REST/v1/Locations/";
