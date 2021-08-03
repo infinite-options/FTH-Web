2e54d67ea82d0d2e76088861c49ace71d7bd1ab1
preeths-r <prangamg@ucsc.edu>
2021-08-02 20:16:47 -0700

    Register page text boxes

--- client/src/components/ServingNow/Register.jsx
+++ client/src/components/ServingNow/Register.jsx
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import "../ServingNow/Register.css";
 import shopping from '../Assets/shopping.png';
 import box from '../Assets/loginBox.png';
-import textBox from '../Assets/textBox.svg';
-import smallTextBox from '../Assets/smallBox1.svg';
 import continuebutton from '../Assets/continue.svg';
 import downArrow from '../Assets/downArrow.svg';
 
@@ -14,15 +12,6 @@ class Login extends Component {
             <div style={{width:'100%'}}>
                 <img src={shopping} alt="" class="shopping"/>
                 <img src={box} alt="" class="box1"/>
-                <img src={textBox} alt="" class="textBox3"/>
-                <img src={textBox} alt="" class="textBox4"/>
-                <img src={textBox} alt="" class="textBox5"/>
-                <img src={textBox} alt="" class="textBox6"/>
-                <img src={smallTextBox} alt="" class="textBox7"/>
-                <img src={smallTextBox} alt="" class="textBox8"/>
-                <img src={textBox} alt="" class="textBox9"/>
-                <img src={smallTextBox} alt="" class="textBox11"/>
-                <img src={smallTextBox} alt="" class="textBox12"/>
                 <img src={continuebutton} alt="" class="continue"/>
                 <img src={downArrow} alt="" class="downArrow"/>
 
@@ -36,8 +25,98 @@ class Login extends Component {
                     <h5> <b>Registration</b></h5>
                 </div>
 
+                <div class="FirstName">
+                  <span class="FirstNameInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "280px"}}
+                      type='text'
+                      placeholder='First Name (required)'
+                    />
+                    </span>
+                </div>
+
+                <div class="LastName">
+                  <span class="LastNameInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "280px"}}
+                      type='text'
+                      placeholder='Last Name (required)'
+                    />
+                    </span>
+                </div>
+
+                <div class="PhoneNumber">
+                  <span class="PhoneNumberInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "280px"}}
+                      type='text'
+                      placeholder='Phone Number'
+                    />
+                    </span>
+                </div>
+
+                <div class="Affiliation">
+                  <span class="AffiliationInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "280px"}}
+                      type='text'
+                      placeholder='School / Affiliation'
+                    />
+                    </span>
+                </div>
+
+                <div class="IDtype">
+                  <span class="IDtypeInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "60px"}}
+                      type='text'
+                      placeholder='ID Type'
+                    />
+                    </span>
+                </div>
+
+                <div class="IDnum">
+                  <span class="IDnumInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "100px"}}
+                      type='text'
+                      placeholder='ID Number'
+                    />
+                    </span>
+                </div>
+
                 <div class="title2">
                     <h5> <b>For Delivery Only</b></h5>
+                </div>
+
+                <div class="Address">
+                  <span class="AddressInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "280px"}}
+                      type='text'
+                      placeholder='Current Address'
+                    />
+                    </span>
+                </div>
+
+                <div class="City">
+                  <span class="CityInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "100px"}}
+                      type='text'
+                      placeholder='City'
+                    />
+                    </span>
+                </div>
+
+                <div class="ZipCode">
+                  <span class="ZipCodeInput">
+                    <input
+                      style={{marginBottom: "0px", border: "0px", width: "120px"}}
+                      type='text'
+                      placeholder='ID Number'
+                    />
+                    </span>
                 </div>
 
             </div>
